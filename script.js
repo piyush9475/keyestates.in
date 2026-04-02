a618d4f190bbec335693322e81063bc88313e20d
sidblue7-ai <sidblue7@gmail.com>
2026-04-02 10:12:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: "smooth"
-  });
-}
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth"
+    });
+  }
