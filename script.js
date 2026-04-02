0c303deed2a9bf8a32e004c078347d4d031f5863
sidblue7-ai <sidblue7@gmail.com>
2026-04-02 16:43:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({
       behavior: "smooth"
     });
   }
+const track = document.querySelector(".card-track");
+const cards = document.querySelectorAll(".card");
+
+let index = 1;
+const cardWidth = 280; // width + gap
+
+function updateSlider() {
+  track.style.transform = `translateX(-${index * cardWidth}px)`;
+
+  document.querySelectorAll(".card").forEach(card => {
+    card.classList.remove("active");
+  });
+
+  cards[index].classList.add("active");
+}
+
+function slide() {
+  index++;
+  track.style.transition = "0.5s";
+
+  updateSlider();
+
+  if (index === cards.length - 1) {
+    setTimeout(() => {
+      track.style.transition = "none";
+      index = 1;
+      updateSlider();
+    }, 500);
+  }
+}
+
+setInterval(slide, 2000);
+
+// Initial center
+updateSlider();
