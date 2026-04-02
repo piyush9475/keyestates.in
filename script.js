1f575180f8c35b1d2e272448b348ddc3f2ad59e0
sidblue7-ai <sidblue7@gmail.com>
2026-04-02 17:38:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-      behavior: "smooth"
-    });
-  }
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
 const track = document.querySelector(".card-track");
 const cards = document.querySelectorAll(".card");
 
@@ -11,28 +11,28 @@ let index = 1;
 const cardWidth = 280; // width + gap
 
 function updateSlider() {
-  track.style.transform = `translateX(-${index * cardWidth}px)`;
+track.style.transform = `translateX(-${index * cardWidth}px)`;
 
-  document.querySelectorAll(".card").forEach(card => {
-    card.classList.remove("active");
-  });
+document.querySelectorAll(".card").forEach(card => {
+  card.classList.remove("active");
+});
 
-  cards[index].classList.add("active");
+cards[index].classList.add("active");
 }
 
 function slide() {
-  index++;
-  track.style.transition = "0.5s";
+index++;
+track.style.transition = "0.5s";
 
-  updateSlider();
+updateSlider();
 
-  if (index === cards.length - 1) {
-    setTimeout(() => {
-      track.style.transition = "none";
-      index = 1;
-      updateSlider();
-    }, 500);
-  }
+if (index === cards.length - 1) {
+  setTimeout(() => {
+    track.style.transition = "none";
+    index = 1;
+    updateSlider();
+  }, 500);
+}
 }
 
 setInterval(slide, 2000);
